8cc4cff18dd2e7f6edb2ea76c9a1c512c8a96822
Tolga Cangöz <mtcangoz@gmail.com>
2025-12-22 12:52:44 +0300

    [MAX] Rename `z_image` module to `z_image_module_v3`
    Update the architecture module name from `z_image` to `z_image_module_v3` to reflect a new version of the image module implementation.
    
    Changes the import statement in the architecture registry to reference the renamed module while maintaining the same `z_image_arch` export.

--- max/python/max/pipelines/architectures/__init__.py
+++ max/python/max/pipelines/architectures/__init__.py
@@ -48,7 +48,7 @@ def register_all_models() -> None:
     from .qwen2_5vl import qwen2_5_vl_arch
     from .qwen3 import qwen3_arch
     from .qwen3vl_moe import qwen3vl_moe_arch
-    from .z_image import z_image_arch
+    from .z_image_module_v3 import z_image_arch
 
     architectures = [
         exaone_arch,
