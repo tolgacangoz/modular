6a2b002674d9c01412a555d19a3722595a59123e
Tolga Cangöz <mtcangoz@gmail.com>
2026-02-07 19:53:11 +0300

    asd

--- max/examples/diffusion/simple_offline_video_generation.py
+++ max/examples/diffusion/simple_offline_video_generation.py
@@ -40,12 +40,17 @@ from max.interfaces import (
 )
 from max.interfaces.provider_options import (
     ProviderOptions,
+    VideoProviderOptions,
 )
 from max.interfaces.request import OpenResponsesRequest
 from max.interfaces.request.open_responses import OpenResponsesRequestBody
 from max.pipelines import PipelineConfig
+from max.pipelines.architectures.ltx2.pipeline_ltx2 import (
+    LTX2Pipeline,
+)
 from max.pipelines.core import PixelContext
 from max.pipelines.lib import PixelGenerationTokenizer
+from max.pipelines.lib.config_enums import SupportedEncoding
 from max.pipelines.lib.pipeline_variants.pixel_generation import (
     PixelGenerationPipeline,
 )
