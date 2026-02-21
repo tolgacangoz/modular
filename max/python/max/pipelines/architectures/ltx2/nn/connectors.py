12ebf9f5648a8df972c5ba242ed3bc269630847e
Tolga Cangöz <mtcangoz@gmail.com>
2026-02-21 23:08:04 +0300

    asd

--- max/python/max/pipelines/architectures/ltx2/nn/connectors.py
+++ max/python/max/pipelines/architectures/ltx2/nn/connectors.py
@@ -271,7 +271,7 @@ class LTX2ConnectorTransformer1d(
                 device=binary_attn_mask.device,
             )
             flipped_mask = (
-                F.gather(binary_attn_mask, reverse_indices, axis=1)
+                binary_attn_mask[:, reverse_indices]
                 .unsqueeze(-1)
                 .cast(padded_hidden_states.dtype)
             )
